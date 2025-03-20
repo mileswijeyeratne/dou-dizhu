4cc28dd031f4b1bf175105dd4bc1fdf90b03d899
Miles Wijeyeratne <milesjpwijeyeratne@gmail.com>
2025-03-20 10:15:55 +0000

    added play hand button and selecting cards

--- frontend/src/components/PlayerHand.tsx
+++ frontend/src/components/PlayerHand.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./PlayerHand.css"
 import { CardType } from "../types/Card";
@@ -13,22 +13,37 @@ interface PlayerHandProps {
 const PlayerHand: React.FC<PlayerHandProps> = ({ cards }) => {
     const cardsWidth = cards.length * CARD_WIDTH - (cards.length - 1) * CARD_OVERLAP
 
+    const [isSelected, setIsSelected] = useState<boolean[]>(new Array(cards.length).fill(false));
+
     return (
         <div className="player-hand">
             <h2>Your Hand</h2>
-            <div className="hand-cards" style={{minWidth: `${cardsWidth}px`}}>
+            <div 
+                className="hand-cards"
+                style={{minWidth: `${cardsWidth}px`}}
+            >
                 {cards.map((card, index) => (
                     <div
                         key={index}  // TODO icl this should change
-                        className="card-wrapper"
+                        className={`card-wrapper ${isSelected[index] ? "selected" : ""}`}
                         style={{
                             left: `${index * (CARD_WIDTH - CARD_OVERLAP)}px`
+                        }}
+                        onClick={() => {
+                            setIsSelected(isSelected.map((val, i) => (
+                                i == index ? !val : val
+                            )));
                         }}
                     >
                         <Card faceDown={false} cardType={card}/>                    
                     </div>
                 ))}
             </div>
+            <button
+                onClick={() => console.log(isSelected)}
+            >
+            Play hand
+            </button>
         </div>
     )
 }
